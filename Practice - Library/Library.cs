37fc3b4f9ec2a7e0a4fc83868a1f541a8c376f4e
FurkanCsk <furkancoskun99@hotmail.com>
2024-08-05 23:14:32 +0300

    Add project files.

---
+++ Practice - Library/Library.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice___Library
+{
+    public class Library
+    {
+        private string _bookName;
+        private string _authorName;
+        private string _authorSurname;
+        private string _publisher;
+        private int _page;
+        private DateTime _date;
+
+        public string bookName  // Property for accessing and modifying the _bookName field.
+        { 
+            get
+            {
+                return _bookName;
+            }
+            set
+            {
+                _bookName = value;
+            }
+        }
+        public string authorName // Property for accessing and modifying the _authorName field.
+        {
+            get
+            {
+                return _authorName;
+            }
+            set
+            {
+                _authorName = value;
+            }
+        }
+        public string authorSurname // Property for accessing and modifying the _authorSurname field.
+        {
+            get
+            {
+                return _authorSurname;
+            }
+            set
+            {
+                _authorSurname = value;
+            }
+        }
+        public string publisher // Property for accessing and modifying the _publisher field.
+        {
+            get
+            {
+                return _publisher;
+            }
+            set
+            {
+                _publisher = value;
+            }
+        }
+        public int page // Property for accessing and modifying the _page field.
+        {
+            get
+            {
+                return _page;
+            }
+            set
+            {
+                _page = value;
+            }
+        }
+        public DateTime date // Property for accessing and modifying the _date field.
+        {
+            get
+            {
+                return _date;
+            }
+            set
+            {
+                _date = value;
+            }
+        }
+
+
+        public Library()  // Default Constructor , initializes a new instance of the Library class and sets the registration date to the current date and time.
+        {
+            SaveDate();
+        }
+
+
+        public Library(string bookName, string authorName,string authorSurname, int page, string publisher) // Initializes a new instance of the Library class with specified values and sets the registration date to the current date and time.
+        {
+            _bookName = bookName;
+            _authorName = authorName;
+            _publisher = publisher;
+            _page = page;
+            SaveDate();
+        }
+
+        public void SaveDate() // Method that sets the _date field to the current date and time when called.
+        {
+            _date = DateTime.Now;
+        }
+
+        public void DisplayProduct() // Method that displays the book's information, including its name, author, page count, publisher, and the date it was recorded.
+        {
+            Console.WriteLine($"Book Name: {_bookName}\nAuthor Name: {_authorName} {_authorSurname}\nPage Count: {_page}\nPublisher: {_publisher}\nRecord Date: {_date}\n");
+        }
+
+    }
+}
---
+++ Practice - Library/Program.cs
@@ -0,0 +1,39 @@
+﻿
+
+// Example of creating and displaying book objects
+
+using Practice___Library;
+
+Library book1 = new Library();
+book1 = new Library("Adı Aylin","Ayşe","Kulin",398,"Remzi Kitabevi");
+
+Library book2 = new Library();
+book2 = new Library("Bir Kitap", "Ayşe", "Yılmaz", 320, "Alfa Yayınları");
+
+Library book3 = new Library();
+book3.bookName = "Kaderin Çocukları";
+book3.authorName = "Darren";
+book3.authorSurname = "Shan";
+book3.page = 200;
+book3.publisher = "Tudem";
+
+
+book1.DisplayProduct();
+book2.DisplayProduct();
+book3.DisplayProduct();
+
+
+/*
+Kodlarınızın altına birer yorum satırıyla örnek üzerinden Class , Property , New , Constructor kavramlarını açıklayınız.
+  
+   * Class :  Verileri ve fonksiyonları içeren alandır. Bu örnekte değişkenleri ve bu değişkenlerle işlem yapan metotları içeren yapıdır.
+   * Property :  Field'ların dışarıdan erişilebilmesini kontrol etmeye yarar. Bu örnekte private olan değişkenleri property kullanılarak erişilebilir yapıldı.
+   * New : OOP'de yeni bir nesne oluşturmak için kullanılan anahtardır.
+   * Constructor : Sınıf içinde kullanılan bir nesnenin yapısını oluşturan yapıcı methoddur.
+   
+
+   * Class: A structure that contains data and functions. In this example, it is the structure that includes variables and methods that operate on these variables.
+   * Property: Allows controlled access to the fields. In this example, properties are used to make private variables accessible.
+   * New: The keyword used to create a new instance of an object in OOP (Object-Oriented Programming).
+   * Constructor: A special method used to set up the structure of an object within a class.
+*/
